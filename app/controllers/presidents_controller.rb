6d804df08ea8372e05af2cc9f3b9fdf9c66c6cd5
kmarks2013 <kmarks2013@gmail.com>
2020-05-29 17:55:37 -0400

    adds show method for president model

--- app/controllers/presidents_controller.rb
+++ app/controllers/presidents_controller.rb
@@ -3,4 +3,10 @@ class PresidentsController < ApplicationController
     def index
         render json: President.all
     end
+
+    def show
+        president = President.find(params[:id])
+        render json: president
+    end
+
 end
