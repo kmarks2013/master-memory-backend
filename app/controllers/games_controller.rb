7cd0e454a94ea4d4a4cb4fd62830dc26bb0eab85
kmarks2013 <kmarks2013@gmail.com>
2020-05-29 19:56:09 -0400

    copies in show and index methods and changes them to game model

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,10 @@
 class GamesController < ApplicationController
+    def index
+        render json: Game.all
+    end
+
+    def show
+        game = Game.find(params[:id])
+        render json:game
+    end
 end
