0e02fec87d9ce6e2b4aae107423093c555e11e62
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-05-29 20:26:23 -0400

    adds show method to game president model

--- app/controllers/game_presidents_controller.rb
+++ app/controllers/game_presidents_controller.rb
@@ -2,4 +2,9 @@ class GamePresidentsController < ApplicationController
     def index
         render json: GamePresident.all
     end
+
+    def show
+        game_president = GamePresident.find(params[:id])
+        render json:game_president
+    end
 end
