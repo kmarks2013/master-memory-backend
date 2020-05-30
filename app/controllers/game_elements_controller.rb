8248b8a80fdda9cf5cad94078b4fa8d527141fae
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-05-29 20:22:31 -0400

    adds show  method to game element  model

--- app/controllers/game_elements_controller.rb
+++ app/controllers/game_elements_controller.rb
@@ -2,4 +2,9 @@ class GameElementsController < ApplicationController
     def index
         render json: GameElement.all
     end
+
+    def show
+        game_element = GameElement.find(params[:id])
+        render json:game_element
+    end
 end
