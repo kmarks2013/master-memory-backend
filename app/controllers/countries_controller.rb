f19d16e4c826f502572e662e012ff0b9eb168a89
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-05-29 20:10:52 -0400

    adds show method to country model

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,4 +2,9 @@ class CountriesController < ApplicationController
     def index
         render json: Country.all
     end
+
+    def show
+        country = Country.find(params[:id])
+        render json: country
+    end
 end
