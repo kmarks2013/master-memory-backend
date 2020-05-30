5e505c347fb465fd271e02abd9008aade7851898
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-05-29 23:17:18 -0400

    adds more attributes for element serializer

--- app/serializers/element_serializer.rb
+++ app/serializers/element_serializer.rb
@@ -1,3 +1,3 @@
 class ElementSerializer < ActiveModel::Serializer
-  attributes :id, :name, :color, :symbol, :atomic_weight, :atomic_number
+  attributes :id, :name, :color, :symbol, :atomic_weight, :atomic_number, :bonding_type, :group_block, :standard_state, :boiling_point, :melting_point, :year_discovered
 end
