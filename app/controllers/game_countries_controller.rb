e96b9e271f7ff618baa9a6de4ccab39e5f1a8df2
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-05-29 20:16:25 -0400

    adds create method to game country model

--- app/controllers/game_countries_controller.rb
+++ app/controllers/game_countries_controller.rb
@@ -7,4 +7,9 @@ class GameCountriesController < ApplicationController
         game = Game.find(params[:id])
         render json:game
     end
+
+    def create
+        game = Game.create(game_params)
+        render json: game
+    end
 end
