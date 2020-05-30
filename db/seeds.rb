82b8a6e68dc2157b28dc4100a1abc963384f2286
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-05-29 22:47:46 -0400

    adds reset_pk_sequence method for User, Game, Country, President models in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,8 @@ Game.destroy_all  #delete this later
 Country.destroy_all
 President.destroy_all
 
+User.reset_pk_sequence
+Game.reset_pk_sequence
+Country.reset_pk_sequence
+President.reset_pk_sequence
+
