4b3159a220cb750c2065d85d5e7b4061495b56e7
kmarks2013 <kmarks2013@gmail.com>
2020-05-29 20:05:08 -0400

    copies in crud from game to game_numbers and updates them for the model

--- app/controllers/game_numbers_controller.rb
+++ app/controllers/game_numbers_controller.rb
@@ -1,2 +1,25 @@
 class GameNumbersController < ApplicationController
+    def index
+        render json: GameNumber.all
+    end
+
+    def show
+        game_number = GameNumber.find(params[:id])
+        render json:game_number
+    end
+
+    def create
+        game_number = GameNumber.create(game_numbers_params)
+        render json: game_number
+    end
+
+    def destroy
+        game_number = GameNumber.find([:id])
+        game_number.destroy
+    end
+
+    private
+    def game_numbers_params
+        params.permit(:game, :number)
+    end
 end
