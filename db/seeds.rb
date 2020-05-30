5f094ea5ed0faf40a0a632413c68ad3293fbf706
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-05-29 23:39:52 -0400

    adds destroy_all and reset_pk_sequence methods to element, and number

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,15 @@ User.destroy_all #delete this later
 Game.destroy_all  #delete this later
 Country.destroy_all
 President.destroy_all
+Element.destroy_all
+Number.destroy_all
 
 User.reset_pk_sequence
 Game.reset_pk_sequence
 Country.reset_pk_sequence
 President.reset_pk_sequence
+Element.reset_pk_sequence
+Number.reset_pk_sequence
 
 country_json = RestClient.get('https://restcountries.eu/rest/v2/all')
 
