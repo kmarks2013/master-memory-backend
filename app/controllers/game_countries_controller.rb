d3f27129413679e4088a3424b86632f3b0271c8b
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-05-29 20:16:03 -0400

    adds show method to game country model

--- app/controllers/game_countries_controller.rb
+++ app/controllers/game_countries_controller.rb
@@ -2,4 +2,9 @@ class GameCountriesController < ApplicationController
     def index
         render json: GameCountry.all
     end
+
+    def show
+        game = Game.find(params[:id])
+        render json:game
+    end
 end
