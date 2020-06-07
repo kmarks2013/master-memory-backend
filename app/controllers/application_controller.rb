5b11dcafa2bd9f691eaa9cca0f4236f22c75dd41
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-06-06 23:31:55 -0400

    adds user_payload method for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::API
     def encode_token(payload)
         JWT.encode(payload, hmac_secret, "HS256")
     end
+
+    def user_payload(user)
+        {user_id: user.id}
+    end
 end
