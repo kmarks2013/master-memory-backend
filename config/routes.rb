a355e04048aec5e29a181524be90500b510ca367
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-06-06 22:56:19 -0400

    adds custom signup route for users

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :game_numbers
   resources :numbers
   resources :games
-  resources :users
+  resources :users, only: [:index, :show, :update, :destroy]
+  post '/signup', to: 'users#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
